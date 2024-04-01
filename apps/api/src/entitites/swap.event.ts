77826bd320745cbe34009c1579255a85fdf8a8a9
Gabriel Matei <gabrielvmatei@gmail.com>
2024-04-01 15:12:01 +0300

    add log

--- apps/api/src/entitites/swap.event.ts
+++ apps/api/src/entitites/swap.event.ts
@@ -46,44 +46,50 @@ export class SwapEvent {
   metadata?: Record<string, string>;
 
   static fromXExchangeSwapEvent(event: XExchangeSwapEvent): SwapEvent {
-    let asset0In: string | undefined = undefined;
-    let asset1In: string | undefined = undefined;
-    let asset0Out: string | undefined = undefined;
-    let asset1Out: string | undefined = undefined;
-    let asset0Reserves: string;
-    let asset1Reserves: string;
-    let priceNative: string;
+    try {
+      let asset0In: string | undefined = undefined;
+      let asset1In: string | undefined = undefined;
+      let asset0Out: string | undefined = undefined;
+      let asset1Out: string | undefined = undefined;
+      let asset0Reserves: string;
+      let asset1Reserves: string;
+      let priceNative: string;
 
-    if (event.pair.firstTokenId === event.tokenInId) {
-      asset0In = new BigNumber(event.tokenInAmount).shiftedBy(-event.pair.firstTokenDecimals).toFixed();
-      asset1Out = new BigNumber(event.tokenOutAmount).shiftedBy(-event.pair.secondTokenDecimals).toFixed();
-      asset0Reserves = new BigNumber(event.tokenInReserves).shiftedBy(-event.pair.firstTokenDecimals).toFixed();
-      asset1Reserves = new BigNumber(event.tokenOutReserves).shiftedBy(-event.pair.secondTokenDecimals).toFixed();
-      priceNative = new BigNumber(asset1Reserves).dividedBy(asset0Reserves).toFixed();
-    } else {
-      asset1In = new BigNumber(event.tokenInAmount).shiftedBy(-event.pair.secondTokenDecimals).toFixed();
-      asset0Out = new BigNumber(event.tokenOutAmount).shiftedBy(-event.pair.firstTokenDecimals).toFixed();
-      asset0Reserves = new BigNumber(event.tokenOutReserves).shiftedBy(-event.pair.firstTokenDecimals).toFixed();
-      asset1Reserves = new BigNumber(event.tokenInReserves).shiftedBy(-event.pair.secondTokenDecimals).toFixed();
-      priceNative = new BigNumber(asset1Reserves).dividedBy(asset0Reserves).toFixed();
+      if (event.pair.firstTokenId === event.tokenInId) {
+        asset0In = new BigNumber(event.tokenInAmount).shiftedBy(-event.pair.firstTokenDecimals).toFixed();
+        asset1Out = new BigNumber(event.tokenOutAmount).shiftedBy(-event.pair.secondTokenDecimals).toFixed();
+        asset0Reserves = new BigNumber(event.tokenInReserves).shiftedBy(-event.pair.firstTokenDecimals).toFixed();
+        asset1Reserves = new BigNumber(event.tokenOutReserves).shiftedBy(-event.pair.secondTokenDecimals).toFixed();
+        priceNative = new BigNumber(asset1Reserves).dividedBy(asset0Reserves).toFixed();
+      } else {
+        asset1In = new BigNumber(event.tokenInAmount).shiftedBy(-event.pair.secondTokenDecimals).toFixed();
+        asset0Out = new BigNumber(event.tokenOutAmount).shiftedBy(-event.pair.firstTokenDecimals).toFixed();
+        asset0Reserves = new BigNumber(event.tokenOutReserves).shiftedBy(-event.pair.firstTokenDecimals).toFixed();
+        asset1Reserves = new BigNumber(event.tokenInReserves).shiftedBy(-event.pair.secondTokenDecimals).toFixed();
+        priceNative = new BigNumber(asset1Reserves).dividedBy(asset0Reserves).toFixed();
+      }
+
+      return {
+        eventType: "swap",
+        txnId: event.txHash,
+        txnIndex: event.txOrder,
+        eventIndex: event.eventOrder,
+        maker: event.caller,
+        pairId: event.address,
+        asset0In,
+        asset1In,
+        asset0Out,
+        asset1Out,
+        priceNative,
+        reserves: {
+          asset0: asset0Reserves,
+          asset1: asset1Reserves,
+        },
+      };
+    } catch (e) {
+      console.log("Error parsing swap event", event);
+      // console.log(e);
+      throw e;
     }
-
-    return {
-      eventType: "swap",
-      txnId: event.txHash,
-      txnIndex: event.txOrder,
-      eventIndex: event.eventOrder,
-      maker: event.caller,
-      pairId: event.address,
-      asset0In,
-      asset1In,
-      asset0Out,
-      asset1Out,
-      priceNative,
-      reserves: {
-        asset0: asset0Reserves,
-        asset1: asset1Reserves,
-      },
-    };
   }
 }
