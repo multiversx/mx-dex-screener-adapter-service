121ebe7d9ed140696c9872f0d6f03ea4889bb835
Gabriel Matei <gabrielvmatei@gmail.com>
2024-09-29 18:57:39 +0300

    warm onedex keys

--- apps/offline-jobs/src/crons/cache-warmer/cache-warmer.module.ts
+++ apps/offline-jobs/src/crons/cache-warmer/cache-warmer.module.ts
@@ -3,12 +3,14 @@ import { CacheWarmerService } from "./cache-warmer.service";
 import { DynamicModuleUtils, MultiversXApiModule, XExchangeModule } from "@mvx-monorepo/common";
 import configuration from "../../../config/configuration";
 import { ScheduleModule } from "@nestjs/schedule";
+import { OneDexModule } from "@mvx-monorepo/common/providers";
 
 @Module({
   imports: [
     ScheduleModule.forRoot(),
     DynamicModuleUtils.getCachingModule(configuration),
     XExchangeModule.forRoot(configuration),
+    OneDexModule.forRoot(configuration),
     MultiversXApiModule.forRoot(configuration),
   ],
   providers: [
--- apps/offline-jobs/src/crons/cache-warmer/cache-warmer.service.ts
+++ apps/offline-jobs/src/crons/cache-warmer/cache-warmer.service.ts
@@ -1,6 +1,7 @@
 import { CacheService } from "@multiversx/sdk-nestjs-cache";
-import { Lock, OriginLogger } from "@multiversx/sdk-nestjs-common";
+import { BatchUtils, Lock, OriginLogger } from "@multiversx/sdk-nestjs-common";
 import { CacheInfo, MultiversXApiService, XExchangeService } from "@mvx-monorepo/common";
+import { OneDexService } from "@mvx-monorepo/common/providers";
 import { Inject, Injectable } from "@nestjs/common";
 import { ClientProxy } from "@nestjs/microservices";
 import { Cron, CronExpression } from "@nestjs/schedule";
@@ -11,13 +12,14 @@ export class CacheWarmerService {
 
   constructor(
     private readonly xExchangeService: XExchangeService,
+    private readonly oneDexService: OneDexService,
     private readonly multiversXApiService: MultiversXApiService,
     private readonly cacheService: CacheService,
     @Inject('PUBSUB_SERVICE') private clientProxy: ClientProxy,
   ) { }
 
   @Cron(CronExpression.EVERY_10_SECONDS)
-  @Lock({ name: 'warmPairsMetadata', verbose: true })
+  @Lock({ name: 'Warm xExchange pairs metadata', verbose: true })
   async warmPairsMetadata() {
     const pairsMetadata = await this.xExchangeService.getPairsMetadataRaw();
 
@@ -25,30 +27,50 @@ export class CacheWarmerService {
     this.clientProxy.emit('deleteCacheKeys', [CacheInfo.PairsMetadata().key]);
   }
 
-  @Cron(CronExpression.EVERY_HOUR)
-  @Lock({ name: 'warmTokens', verbose: true })
-  async warmTokens() {
-    const pairsMetadata = await this.xExchangeService.getPairsMetadata();
-    const tokens = pairsMetadata.map(pair => [pair.firstTokenId, pair.secondTokenId]).flat().distinct();
+  @Cron(CronExpression.EVERY_10_MINUTES)
+  @Lock({ name: 'Warm OneDex pairs metadata', verbose: true })
+  async warmOneDexPairsMetadata() {
+    const pairsMetadata = await this.oneDexService.getPairsMetadataRaw();
 
-    const cacheKeys: string[] = [];
-    for (const tokenId of tokens) {
-      try {
-        const token = await this.multiversXApiService.getTokenRaw(tokenId);
-
-        await this.cacheService.set(CacheInfo.Token(tokenId).key, token, CacheInfo.Token(tokenId).ttl);
-        cacheKeys.push(CacheInfo.Token(tokenId).key);
-      } catch (error: any) {
-        this.logger.error(`Failed to get token with identifier: ${tokenId}`);
-        this.logger.error(error);
-      }
-    }
-
-    this.clientProxy.emit('deleteCacheKeys', cacheKeys);
+    await this.cacheService.set(CacheInfo.OneDexPairsMetadata().key, pairsMetadata, CacheInfo.OneDexPairsMetadata().ttl);
+    this.clientProxy.emit('deleteCacheKeys', [CacheInfo.OneDexPairsMetadata().key]);
   }
 
-  @Cron(CronExpression.EVERY_10_MINUTES)
-  @Lock({ name: 'warmPairFees', verbose: true })
+  @Cron(CronExpression.EVERY_2ND_HOUR)
+  @Lock({ name: 'Warm Tokens', verbose: true })
+  async warmTokens() {
+    const xExchangePairsMetadata = await this.xExchangeService.getPairsMetadata();
+    const oneDexPairsMetadata = await this.oneDexService.getPairsMetadata();
+
+    const xExchangeTokens = xExchangePairsMetadata.map(pair => [pair.firstTokenId, pair.secondTokenId]).flat();
+    const oneDexTokens = oneDexPairsMetadata.map(pair => [pair.first_token_id, pair.second_token_id]).flat();
+
+    const tokens = [...xExchangeTokens, ...oneDexTokens].distinct();
+    const tokenBatches = BatchUtils.splitArrayIntoChunks(tokens, 25);
+
+    for (const batch of tokenBatches) {
+      const cacheKeys: string[] = [];
+
+      for (const tokenId of batch) {
+        try {
+          const token = await this.multiversXApiService.getTokenRaw(tokenId);
+
+          await this.cacheService.set(CacheInfo.Token(tokenId).key, token, CacheInfo.Token(tokenId).ttl);
+          cacheKeys.push(CacheInfo.Token(tokenId).key);
+        } catch (error: any) {
+          this.logger.error(`Failed to get token with identifier: ${tokenId}`);
+          this.logger.error(error);
+        }
+      }
+
+      this.clientProxy.emit('deleteCacheKeys', cacheKeys);
+
+      await new Promise(r => setTimeout(r, 5000));
+    }
+  }
+
+  @Cron(CronExpression.EVERY_30_MINUTES)
+  @Lock({ name: 'Warm xExchange pair fees', verbose: true })
   async warmPairFees() {
     const pairsMetadata = await this.xExchangeService.getPairsMetadata();
 
